c7281ba5f1b25fff423f9c6ba2f10b7213a8c97f
정예준 <yejun@jeong-yejun-ui-MacBookAir.local>
2025-01-04 20:45:47 +0900

    yejun

--- src/Body/Popular.tsx
+++ src/Body/Popular.tsx
@@ -1,51 +1,43 @@
-
 import React from 'react';
-import { useQuery } from '@tanstack/react-query';
 import '../CSS/Popular.css';
 import FoodCardComponent from '../Components/FoodCardComponent';
-import { API_SRC } from '../API/custom_search_api_service';
+import { useQuery } from '@tanstack/react-query';
 
-const fetchFoodData = async (name: string) => {
-  const response = await fetch(`${API_SRC}${name}`);
+const fetchPopularQuery = async () => {
+  const response = await fetch('http://localhost:4000/services/food/popular');
   if (!response.ok) {
-    console.log('Something went wrong!');
-    throw new Error('Something went wrong!');
+    throw new Error('Network response was not ok');
   }
-  const data = await response.json();
-  return data;
+  return response.json();
 };
 
 const Popular: React.FC = () => {
-  const test_data = ['닭갈비', '샐러드', '피자', '돈까스', '스파게티', '라면', '감자탕', '냉면', '짜장'];
 
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['foodData'],
-    queryFn: async () => {
-      const results = await Promise.all(test_data.map(fetchFoodData));
-      return results;
-    },
+  const { data, error, isLoading, isError } = useQuery({
+    queryKey: ['posts'],  // 데이터 캐시 키
+    queryFn: fetchPopularQuery, // 데이터를 가져오는 함수
   });
 
-  // 로딩 상태 처리
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-  // 에러 상태 처리
-  if (error instanceof Error) return <div>Error: {error.message}</div>;
-
-  const foodCardList = test_data.map((name, idx) => (
-    <FoodCardComponent
-      key={idx}
-      name={name}
-      img_src={(data && data[idx]?.image) || '정보없음'}
-    />
-  ));
-
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+  
+  const data_list = data['item'].map(
+    (item : JSON, idx : number) => 
+      (<FoodCardComponent item={item}></FoodCardComponent>));
   return (
     <div className="app-container">
+      
       {/* 카드 레이아웃 */}
-      <div className="card-container">{foodCardList}</div>
+      <div className="card-container">
+        {data_list}
+      </div>
     </div>
   );
-};
+}
 
 export default Popular;
--- src/Components/FoodCardComponent.tsx
+++ src/Components/FoodCardComponent.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import '../CSS/Popular.css';  // CSS 스타일을 따로 추가할 수 있습니다.
 
-
-interface CardItemProps {
-  name: string;
-  img_src: string;
-  
+interface Food {
+  item: JSON;
 }
 
-const FoodCardComponent : React.FC<CardItemProps> = (props) => {
-  const navigate = useNavigate();
-  
+
+const FoodCardComponent : React.FC<Food> = (props) => {
+  const foodname = JSON.parse(JSON.stringify(props.item))['food_name'];
   return (
     <div className="card">
       <img
-        src={props.name}
+        //src는 테스트 제외로 나중에 추가해주어야함.
+        src="정보없음"
         className="card-image"
       />
       <div className="card-body">
-        <h2 className="card-title">{props.name}</h2>
-        <button className="card-button" onClick={() => navigate("/recipeDetail")}>레시피 보러가기</button>
+        <h2 className="card-title">{foodname}</h2>
+        <button className="card-button">레시피 보러가기</button>
       </div>
     </div>
   );
