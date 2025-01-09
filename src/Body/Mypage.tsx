e24c6a73dc8931190acb4e27132d3cb152bef2c9
taeil <tail01@pusan.ac.kr>
2025-01-09 23:02:15 +0900

    taeil

--- src/Body/Mypage.tsx
+++ src/Body/Mypage.tsx
@@ -1,13 +1,61 @@
-import React, { useState} from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState} from 'react';
+import { data, useNavigate } from 'react-router-dom';
 import { useGlobal, useRefresh } from '../contexts/GlobalContext';
 
-const Mypage: React.FC = () => {
-    return (
-        <div>
-            <h1>마이페이지입니다.</h1>
-        </div>
-    );
+interface User {
+    user_id: string;
+    email: string;
+    preference1: string;
+    preference2: string;
+    preference3: string;
 }
+
+const Mypage: React.FC = () => {
+    const navigate = useNavigate();
+    const { globalVariable, setGlobalVariable } = useGlobal();
+    const { refresh, setRefresh} = useRefresh();
+    const [ user, setUser] = useState<User | null>(null);
+
+    const response = fetch('http://localhost:4000/services/user/mypage',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user_id: globalVariable }),
+    })
+    .then((response) => {
+        if(!response.ok){
+            console.error('HTTP Error:', response.status);
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {
+        setUser(data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+    
+
+    return (
+        <><div>
+            <h1>마이페이지</h1>
+        </div><div>{refresh ?
+            (<div>
+                <h2>{globalVariable}님 안녕하세요!</h2>
+                <div>이메일: {user?.email}</div>
+                <div>선호1: {user?.preference1}</div>
+                <div>선호2: {user?.preference2}</div>
+                <div>선호3: {user?.preference3}</div>
+
+                <button onClick={() => { navigate('/'); } }>홈으로</button>
+                </div>) :
+            (<></>)}</div></>
+        
+    );
+    
+};
 
 export default Mypage;
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -39,7 +39,7 @@ const Header: React.FC = () => {
         <img className='w-64' src={img} alt="냉장고를 부탁해 로고" />
         <p className='text-5xl text-center Header-Logo'>냉장고를 부탁해</p>
         {
-        refresh ? (<p onClick={()=>logout()}>{globalVariable}님, 안녕하세요</p>) :(null)
+        refresh ? (<p>{globalVariable}님, 안녕하세요</p>) :(null)
         }
         
       </div>
@@ -47,13 +47,26 @@ const Header: React.FC = () => {
         <div className={activeMenu === 'ingredient' ? 'active' : 'non-active'} onClick={() => handleMenuClick('/ingredient', 'ingredient')}>재료입력</div>
         <div className={activeMenu === 'taste' ? 'active' : 'non-active'} onClick={() => handleMenuClick('/taste', 'taste')}>추천 레시피 받기</div>
         <div className={activeMenu === 'popular' ? 'active' : 'non-active'} onClick={() => handleMenuClick('/popular', 'popular')}>인기 레시피</div>
+
         <div>
+          {
+            refresh ? 
+              (<div className='flex h-[100%]'>
+                <div className={activeMenu === 'login' ? 'mypage active' : 'mypage non-active'} onClick={() => handleLoginClick('/mypage', 'login')}>마이페이지</div>
+                <div className={activeMenu === 'login' ? 'mypage active' : 'mypage non-active'} onClick={() => logout()}>로그아웃</div>
+              </div>
+              ) : 
+              (<div className={activeMenu === 'login' ? 'login active' : 'login non-active'} onClick={() => handleLoginClick('/login', 'login')}>로그인</div>)
+          }
+        </div>
+
+        {/* <div>
           {
             refresh ? 
               (<div className={activeMenu === 'login' ? 'mypage active' : 'mypage non-active'} onClick={() => handleLoginClick('/mypage', 'mypage')}>내 정보</div>) : 
               (<div className={activeMenu === 'login' ? 'login active' : 'login non-active'} onClick={() => handleLoginClick('/login', 'login')}>로그인</div>)
           }
-        </div>
+        </div> */}
         
         
       </div>
